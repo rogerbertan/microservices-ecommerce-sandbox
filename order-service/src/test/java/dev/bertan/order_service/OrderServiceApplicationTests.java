536f2ef7e9c65dc94cb47b7557d01dd8b74ab518
Roger Bertan <roger.bertan@gmail.com>
2026-05-16 12:43:31 -0300

    chore(order-service): add initial structure spring boot

---
+++ order-service/src/test/java/dev/bertan/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bertan.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
